b2c3c40401d3c68ec938aec30cb475dc32153bfc
Kalab <kalab@vojtechkalab.eu>
2017-01-03 16:40:39 +0100

    init main

---
+++ main.php
@@ -0,0 +1,34 @@
+<?php
+require "vendor/autoload.php";
+
+// read the configuration file
+$dataDir = getenv('KBC_DATADIR') . DIRECTORY_SEPARATOR;
+$configFile = $dataDir . 'config.json';
+$config = json_decode(file_get_contents($configFile), true);
+$multiplier = $config['parameters']['multiplier'];
+// create output file and write header
+$outFile = new \Keboola\Csv\CsvFile(
+    $dataDir . 'out' . DIRECTORY_SEPARATOR . 'tables' . DIRECTORY_SEPARATOR . 'destination.csv'
+);
+$outFile->writeRow(['number', 'someText', 'double_number']);
+// read input file and write rows of output file
+$inFile = new Keboola\Csv\CsvFile($dataDir . 'in' . DIRECTORY_SEPARATOR . 'tables' . DIRECTORY_SEPARATOR . 'source.csv');
+foreach ($inFile as $rowNum => $row) {
+    if ($rowNum == 0) {
+        // skip header
+        continue;
+    }
+    $outFile->writeRow([
+        $row[0],
+        $row[1],
+        $row[0] * $multiplier
+    ]);
+}
+try {
+} catch (InvalidArgumentException $e) {
+    echo $e->getMessage();
+    exit(1);
+} catch (\Throwable $e) {
+    echo $e->getMessage();
+    exit(2);
+}
